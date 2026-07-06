4e89f8ccc1a84e9f512308beb738741cf6b32e45
shivam6497 <kdmboy6397@gmail.com>
2026-07-06 13:55:57 +0000

    feat: Rate-Limiter-Middleware

--- dist/middleware/rateLimiter.d.ts
+++
@@ -1,8 +0,0 @@
-import type { Request, Response, NextFunction } from "express";
-interface RateLimitOptions {
-    maxRequests: number;
-    windowInSeconds: number;
-}
-declare const rateLimiter: (options: RateLimitOptions, name: string) => (req: Request, res: Response, next: NextFunction) => Promise<void>;
-export default rateLimiter;
-//# sourceMappingURL=rateLimiter.d.ts.map
